610b878a1d22864ebab3b899611d9b3c6ce74ae1
Bekjon Ishpulatov <84561206+bekjon-me@users.noreply.github.com>
2022-05-11 23:42:18 +0500

    android safeAreaView fixed using StatusBar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import RN from './app/components/common/react-native';
-import { COLORS } from './app/constants/theme';
-import { DashboardScreen } from './app/screens/private';
+import { StatusBar } from "expo-status-bar";
+import RN from "./app/components/common/react-native";
+import { COLORS } from "./app/constants/theme";
+import { DashboardScreen } from "./app/screens/private";
 // import { AppNavigation } from './app/navigation';
 
 export default function App() {
-	return (
-		<RN.SafeAreaView style={styles.container}>
-			<StatusBar style='auto' />
-			<DashboardScreen />
-		</RN.SafeAreaView>
-	);
+  return (
+    <RN.SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <DashboardScreen />
+    </RN.SafeAreaView>
+  );
 }
 
 const styles = RN.StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: COLORS.gray,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.gray,
+    marginTop: RN.Platform.OS === "android" ? RN.StatusBar.currentHeight : 0,
+  },
 });

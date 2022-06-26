679da8304c5974401f059d49bf62fb86509a9e1d
BekRoz <bekrozikoff@gmail.com>
2022-06-26 21:15:04 +0500

    Add storybook config

---
+++ storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../app/components/**/*.stories.?(ts|tsx|js|jsx)"],
+  addons: [],
+};
---
+++ storybook/rn-addons.js
@@ -0,0 +1,5 @@
+import "@storybook/addon-actions/manager";
+import "@storybook/addon-links/manager";
+import "@storybook/addon-knobs/manager";
+import "@storybook/addon-ondevice-actions/manager";
+import "@storybook/addon-ondevice-knobs/manager";
